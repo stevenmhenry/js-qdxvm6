1411517821c4c7091fdfacd15b89a1bef5e3703c
stevenmhenry <120347489+stevenmhenry@users.noreply.github.com>
2023-02-08 16:39:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-qdxvm6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qdxvm6)
---
+++ index.js
@@ -0,0 +1,57 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+<html>
+    <head>
+        <title>Search Engine Comparison</title>
+            <style>
+                  table, th, td {
+                          border: 1px solid black;
+                                  border-collapse: collapse;
+                                        }
+                                              th, td {
+                                                      padding: 5px;
+                                                              text-align: left;
+                                                                    }
+                                                                        </style>
+                                                                          </head>
+                                                                            <body>
+                                                                                <h1>Search Engine Comparison</h1>
+                                                                                    <table style="width:100%">
+                                                                                          <tr>
+                                                                                                  <th>Search Engine</th>
+                                                                                                          <th>Result 1</th>
+                                                                                                                  <th>Result 2</th>
+                                                                                                                          <th>Result 3</th>
+                                                                                                                                </tr>
+                                                                                                                                      <tr>
+                                                                                                                                              <td>ChatGPT</td>
+                                                                                                                                                      <td>Result 1 from ChatGPT</td>
+                                                                                                                                                              <td>Result 2 from ChatGPT</td>
+                                                                                                                                                                      <td>Result 3 from ChatGPT</td>
+                                                                                                                                                                            </tr>
+                                                                                                                                                                                  <tr>
+                                                                                                                                                                                          <td>Google</td>
+                                                                                                                                                                                                  <td>Result 1 from Google</td>
+                                                                                                                                                                                                          <td>Result 2 from Google</td>
+                                                                                                                                                                                                                  <td>Result 3 from Google</td>
+                                                                                                                                                                                                                        </tr>
+                                                                                                                                                                                                                              <tr>
+                                                                                                                                                                                                                                      <td>Yahoo</td>
+                                                                                                                                                                                                                                              <td>Result 1 from Yahoo</td>
+                                                                                                                                                                                                                                                      <td>Result 2 from Yahoo</td>
+                                                                                                                                                                                                                                                              <td>Result 3 from Yahoo</td>
+                                                                                                                                                                                                                                                                    </tr>
+                                                                                                                                                                                                                                                                          <tr>
+                                                                                                                                                                                                                                                                                  <td>DuckDuckGo</td>
+                                                                                                                                                                                                                                                                                          <td>Result 1 from DuckDuckGo</td>
+                                                                                                                                                                                                                                                                                                  <td>Result 2 from DuckDuckGo</td>
+                                                                                                                                                                                                                                                                                                          <td>Result 3 from DuckDuckGo</td>
+                                                                                                                                                                                                                                                                                                                </tr>
+                                                                                                                                                                                                                                                                                                                    </table>
+                                                                                                                                                                                                                                                                                                                      </body>
+                                                                                                                                                                                                                                                                                                                      </html>
+                                                                                                                                                                                                                                                                                                                      
